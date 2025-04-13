ddf95544fe719c458d4542a685f1146c35f8d439
Ryu-224 <rojo55392@gmail.com>
2025-04-13 13:50:05 +0900

    Add Py File [hw5.py]

---
+++ hw5.py
@@ -0,0 +1,27 @@
+def read_single_digit(n):
+    
+    korean = {0: "공", 1: "하나", 2: "둘", 3: "삼", 4: "넷", 5: "오", 6: "여섯", 7: "칠", 8: "팔", 9: "아홉"}
+    
+    return korean[n]
+
+def read_number(num):
+    if num < 10:
+        return read_single_digit(num)
+    
+    elif num < 100:
+        ten = num // 10
+        one = num % 10
+        return f"{read_single_digit(ten)} {read_single_digit(one)}"
+    
+    elif num < 1000:
+        hundred = num // 100
+        ten = (num // 10) % 10
+        one = num % 10
+        return f"{read_single_digit(hundred)} {read_single_digit(ten)} {read_single_digit(one)}"
+    
+    else:
+        return "오류"
+
+number = int(input("세 자리 정수 입력: "))
+korean_number = read_number(number)
+print(korean_number)
