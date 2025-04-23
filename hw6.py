31c6bdab84c43438b874399be596b131c5032eb7
Ryu-224 <rojo55392@gmail.com>
2025-04-23 17:25:04 +0900

    Add Py File [hw6.py]

---
+++ hw6.py
@@ -0,0 +1,11 @@
+for i in range(1, 10 ,1):
+    print()
+    for j in range(2, 6, 1):
+        print(f"{j} x {i} = {i * j:2d}", end ="\t")
+
+print()
+
+for i in range(1, 10 ,1):
+    print()
+    for j in range(6, 10, 1):
+        print(f"{j} x {i} = {i * j:2d}", end ="\t")
